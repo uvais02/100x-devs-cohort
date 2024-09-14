a75c480e632c4be61d5cce1eae306ff885673d80
uvais02 <mohameduvaismn@gmail.com>
2024-09-14 23:29:11 +0530

    added get controller

--- in_class/http-server-1/index.js
+++ in_class/http-server-1/index.js
@@ -2,8 +2,42 @@ const express = require("express")
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send("Hello World!")
+var users = [{
+    name: 'John',
+    kidneys: [
+        {
+            healthy: false
+        },
+        {
+            healthy: true
+        }
+    ]
+}]
+
+app.get("/", (req, res) => {
+    const n = parseInt(req.query.n)
+
+    if (isNaN(n) || n < 0 || n >= users.length) {
+        return res.status(400).send("Invalid user index");
+    }
+    
+    let healthyKidneys = users[n].kidneys.filter(kidney => kidney.healthy)
+    res.status(200).send({
+        'Total Kidneys'  : users[n].kidneys.length,
+        'Healthy kidneys': healthyKidneys.length
+    });
+})
+
+app.post("/", (req, res) => {
+
+})
+
+app.put("/", (req, res) => {
+
+})
+
+app.delete("/", (req, res) => {
+
 })
 
 const port = 3000
