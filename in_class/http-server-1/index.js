c2de35210a56c2da982c1c7b857da91b2fb6afb9
uvais02 <mohameduvaismn@gmail.com>
2024-09-15 17:39:00 +0530

    added delete controller

--- in_class/http-server-1/index.js
+++ in_class/http-server-1/index.js
@@ -45,7 +45,17 @@ app.put("/", (req, res) => {
 })
 
 app.delete("/", (req, res) => {
+    const n = parseInt(req.query.n)
 
+    if (isNaN(n) || n < 0 || n >= users.length) 
+        return res.status(400).send("Invalid user index");
+
+    if (!users[n].kidneys.find(kidney => kidney.healthy == false))
+        return res.status(400).send("No unhealthy kidneys found!")
+
+    users[n].kidneys = users[n].kidneys.filter(kidney => kidney.healthy)
+
+    res.status(200).send("Removed unhealthy kidneys!")
 })
 
 const port = 3000
