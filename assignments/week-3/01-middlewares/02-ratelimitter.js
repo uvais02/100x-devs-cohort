c0cabd4ce881d3a0addfbac839e229f920f76598
uvais02 <mohameduvaismn@gmail.com>
2024-09-18 23:33:20 +0530

    week-3 middleware ratelimitter

--- assignments/week-3/01-middlewares/02-ratelimitter.js
+++ assignments/week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,22 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use((req, res, next) => {
+  const userId = req.headers['user-id']
+
+  if (numberOfRequestsForUser[userId] == undefined) {
+    numberOfRequestsForUser[userId] = 1
+  }
+  else {
+    numberOfRequestsForUser[userId] += 1
+  }
+
+  if (numberOfRequestsForUser[userId] >= 5) 
+    return res.status(404).send('blocked')
+  
+  next()
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
