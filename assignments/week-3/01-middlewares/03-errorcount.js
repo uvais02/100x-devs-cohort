74fe776ccc581b488ddfae073841c5c87b7fa4b5
uvais02 <mohameduvaismn@gmail.com>
2024-09-18 23:36:39 +0530

    week-3 middleware errorCounter

--- assignments/week-3/01-middlewares/03-errorcount.js
+++ assignments/week-3/01-middlewares/03-errorcount.js
@@ -23,4 +23,11 @@ app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
 
+app.use((err, req, res, next) => {
+  if (err) {
+    errorCount += 1
+    res.status(404).send("Something unexpected happened!")
+  }
+})
+
 module.exports = app;
