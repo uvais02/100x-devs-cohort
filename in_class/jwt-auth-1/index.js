b8ffcdf9e94ce90ad70bfbf5f54ce7c1bb25a29a
uvais02 <mohameduvaismn@gmail.com>
2024-09-21 12:59:22 +0530

    basic jwt auth

--- in_class/jwt-auth-1/index.js
+++ in_class/jwt-auth-1/index.js
@@ -4,6 +4,9 @@ const jwt = require('jsonwebtoken')
 const app = express()
 app.use(express.json())
 
+
+const jwtSecret = 'secretKey'
+
 const ALL_USERS = [
     {
       username: "harkirat@gmail.com",
@@ -20,13 +23,34 @@ const ALL_USERS = [
       password: "123321",
       name: "Priya kumari",
     },
-  ];
+];
+
+function userExists(username, password) {
+    return ALL_USERS.filter(user => user.username == username && user.password == password)
+            .length > 0
+}
 
 /*
   receive username and password through body
   returns a jwt with username encrypted
 */
 app.post('/signin', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    if (!userExists(username, password)) {
+        return res.status(403).json({
+          msg: "User doesn't exist in our in memory db",
+        });
+    }
+
+    var token = jwt.sign({
+        username: username
+    }, jwtSecret)
+
+    return res.status(200).json({
+        Bearer: token
+    })
 
 })
 
@@ -36,5 +60,32 @@ app.post('/signin', (req, res) => {
     else return 403
 */
 app.get('/users', (req, res) => {
+    const token = req.headers.authorization
+    console.log(token)
 
+    try {
+        const v = jwt.verify(token, jwtSecret)
+        const username = v.username
+
+        if (!ALL_USERS.some(user => user.username == username)) {
+            return res.status(401).json({
+                msg: "Unauthorized access"
+            })
+        }
+
+        const filteredUsers = ALL_USERS.filter(user => user.username !== username)
+
+        return res.status(200).json({
+            filteredUsers
+        })
+    } catch(err) {
+        return res.status(401).json({
+            msg: "Invalid token"
+        })
+    }
+
+})
+
+app.listen(3000, () => {
+    console.log('Server started')
 })
