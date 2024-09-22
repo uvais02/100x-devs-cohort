6b4cabf02f809f01fb876b8c510dda452b9dfa7d
uvais02 <mohameduvaismn@gmail.com>
2024-09-22 22:18:31 +0530

    added middlewares

--- assignments/week-3/03-mongo/middleware/admin.js
+++ assignments/week-3/03-mongo/middleware/admin.js
@@ -1,7 +1,18 @@
+const { Admin } = require("../db");
+
 // Middleware for handling auth
-function adminMiddleware(req, res, next) {
+async function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    let {username, password} = req.headers;
+
+    const admin = await Admin.findOne({username, password});
+    if (admin) {
+        next();
+    }
+    res.status(403).json({
+        msg: "Admin doesn't exist"
+    })
 }
 
 module.exports = adminMiddleware;
