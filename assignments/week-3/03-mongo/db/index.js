e3adf675e15b63fbf7383381fc1bc7297ed34fb1
uvais02 <mohameduvaismn@gmail.com>
2024-09-22 22:12:31 +0530

    modified schema

--- assignments/week-3/03-mongo/db/index.js
+++ assignments/week-3/03-mongo/db/index.js
@@ -15,12 +15,15 @@ const AdminSchema = new mongoose.Schema({
 const UserSchema = new mongoose.Schema({
     // Schema definition here
     username: {type: String, required: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
-    id: {type: Number, required: true}, 
     title: {type: String, required: true}, 
     description: {type: String, required: true},
     price: {type: Number, required: true}, 
