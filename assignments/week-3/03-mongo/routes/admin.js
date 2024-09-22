e2ef142b17a94d93a61e9e4675ef83e96f2afb74
uvais02 <mohameduvaismn@gmail.com>
2024-09-22 22:33:39 +0530

    added signup route for admin

--- assignments/week-3/03-mongo/routes/admin.js
+++ assignments/week-3/03-mongo/routes/admin.js
@@ -1,10 +1,25 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin, Course } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement admin signup logic
+    const {username, password} = req.body;
+    try {
+        const existingAdmin = await Admin.findOne({username, password});
+        if (existingAdmin) {
+            return res.status(409).json({ msg: 'Admin already exists!' });
+        }
+
+        const newAdmin = new Admin({username, password});
+        await newAdmin.save();
+        return res.status(201).json({ msg: 'Admin registered successfully' });
+
+    } catch (err) {
+        return res.status(500).json({ error: 'Error registering user' });
+    }
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
