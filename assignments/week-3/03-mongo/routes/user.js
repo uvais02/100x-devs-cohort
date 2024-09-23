d1ea0c4cb6cbe07abd0e448eb174cb8666f3ed7d
uvais02 <mohameduvaismn@gmail.com>
2024-09-23 22:50:35 +0530

    signup endpoint for user

--- assignments/week-3/03-mongo/routes/user.js
+++ assignments/week-3/03-mongo/routes/user.js
@@ -1,10 +1,30 @@
 const { Router } = require("express");
 const router = Router();
 const userMiddleware = require("../middleware/user");
+const {User, Course} = require('../db');
 
 // User Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
     // Implement user signup logic
+    let {username, password} = req.body;
+    try {
+        const existingUser = await User.findOne({username, password});
+        if (existingUser) {
+            return res.status(409).json({
+                msg: "User already exists!"
+            });
+        }
+
+        await User.create({username, password});
+        res.status(201).json({
+            msg: "User created successfully!"
+        });
+    } catch (err) {
+        res.status(500).json({
+            msg: "Error while creating user!"
+        });
+    }
+
 });
 
 router.get('/courses', (req, res) => {
