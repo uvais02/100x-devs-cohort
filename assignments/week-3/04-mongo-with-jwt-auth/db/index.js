bd01efe325f889a98ce39a7f6f58a9e37cdbb2c5
uvais02 <mohameduvaismn@gmail.com>
2024-11-09 16:11:35 +0530

    week3 jwt with mongodb - setting up schema

--- assignments/week-3/04-mongo-with-jwt-auth/db/index.js
+++ assignments/week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,34 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+MONGO_DB = 'week3_mongodb_jwt'
+mongoose.connect(process.env.MONGO_URI + MONGO_DB);
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: {type: String, required: true},
+    password: {type: String, required: true}
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: {type: String, required: true}, 
+    description: {type: String, required: true},
+    price: {type: Number, required: true}, 
+    imageLink: {type: String, required: true}, 
+    published: {type: Boolean, required: true}
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
