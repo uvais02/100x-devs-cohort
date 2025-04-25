5f304025e89f0ef4dc9bb12d5da0330a610e23f3
uvais02 <mohameduvaismn@gmail.com>
2025-04-26 00:18:56 +0530

    simple todo app

--- assignments/week-6/simple-todo/src/App.jsx
+++ assignments/week-6/simple-todo/src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [todos, setTodos] = useState([
+        {title: 'Todo 1', description: 'Description 1'},
+        {title: 'Todo 2', description: 'Description 2'},
+        {title: 'Todo 3', description: 'Description 3'},
+    ])
 
-  return (
-    <>
-    </>
-  )
+    const addTodo = () => {
+        const newTodo = {
+            title: `Todo ${todos.length + 1}`,
+            description: `Description ${todos.length + 1}`
+        }
+        setTodos([...todos, newTodo])
+    }
+
+    return (
+        <>
+            <button onClick={addTodo}>Add TODO</button>
+            {todos.map((todo, index) => <Todo key={index} title={todo.title} description={todo.description} />)}
+        </>
+    )
+}
+
+function Todo({title, description}) {
+    return (
+        <div className="todo">
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+    )
 }
 
 export default App
