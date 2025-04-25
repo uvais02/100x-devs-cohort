cef98e8f35df1158075262cdb8cea41b5e629b93
uvais02 <mohameduvaismn@gmail.com>
2025-04-26 00:07:23 +0530

    Trying react memo to avoid unnecessaray rerenders

--- assignments/week-6/week-6-experiments/src/App.jsx
+++ assignments/week-6/week-6-experiments/src/App.jsx
@@ -1,34 +1,29 @@
+import React from 'react'
 import { useState } from 'react'
 import './App.css'
 
 function App() {
-  return (
-    <>
-      <HeaderWithButton />
-      <Header title={'header2'}/>
-      <Header title={'header3'}/>
-      <Header title={'header4'}/>
-    </>
-  )
-}
-
-function Header({title}) {
-  return <div>
-    {title}
-  </div>
-}
-
-function HeaderWithButton() {
   const [firstHeaderState, setFirstHeaderState] = useState("header1")
 
   function handleClick() {
     setFirstHeaderState("header" + Math.random())
   }
 
-  return <>
-    <button onClick={handleClick}>click me to change the title</button>
-    <Header title={firstHeaderState}/>
-  </>
+  return (
+    <>
+      <button onClick={handleClick}>click me to change the title</button>
+      <Header title={firstHeaderState}/>      
+      <Header title={'header2'}/>
+      <Header title={'header2'}/>
+      <Header title={'header2'}/>
+    </>
+  )
 }
+
+const Header = React.memo(function Header({title}) {
+  return <div> 
+    {title}
+  </div>
+})
 
 export default App
